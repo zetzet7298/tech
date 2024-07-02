03c92fdf9c367c179efbb0c560b8c304484de4dc
Dang Bui <phudang@tuoitre.com.vn>
2024-07-02 16:52:59 +0700

    update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\CountVisitors::class,
+        // \App\Http\Middleware\CountVisitors::class,
     ];
 
     /**
